738646ee352588d5db3f44f41e42d75f303dddf5
mikee1897 <33421122+mikee1897@users.noreply.github.com>
2019-07-30 14:47:19 +0800

    up

--- profiles/populate_db.py
+++ profiles/populate_db.py
@@ -980,7 +980,13 @@ def generate_dogbreed():
         randomizer = random.randint(0, 4)
         temperament = temperament_list[randomizer]
 
-        Dog_Breed.objects.create(breed=data,life_span=10,temperament=temperament,colors=generate_skin_color(), weight=20,male_height=10,female_height=10,skill_recommendation='NDD',skill_recommendation2='EDD',skill_recommendation3='SAR',litter_number=7,value=15000)
+        Dog_Breed.objects.create(breed=data,sex='Male',life_span=10,temperament=temperament,colors=generate_skin_color(), weight=20,male_height=10,female_height=10,skill_recommendation='NDD',skill_recommendation2='EDD',skill_recommendation3='SAR',litter_number=7,value=15000)
+		
+	for data in arr:
+        randomizer = random.randint(0, 4)
+        temperament = temperament_list[randomizer]
+
+        Dog_Breed.objects.create(breed=data,sex='Female',life_span=10,temperament=temperament,colors=generate_skin_color(), weight=20,male_height=10,female_height=10,skill_recommendation='NDD',skill_recommendation2='EDD',skill_recommendation3='SAR',litter_number=7,value=17000)
 
     return None
 
